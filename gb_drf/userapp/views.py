a994ba536ffe6fbdea36d24549c38a7b41c282cf
mbackReserved <lowmustgoon@gmail.com>
2023-01-12 20:30:58 +0300

    create viewset and router

--- gb_drf/userapp/views.py
+++ gb_drf/userapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .serializers import UserModelSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import User
 
-# Create your views here.
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
